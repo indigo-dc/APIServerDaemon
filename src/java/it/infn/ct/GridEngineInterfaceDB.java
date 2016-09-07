f94411b9c88106f6fbfb63fa55b3db81f01fa660
Riccardo Bruno <ricsxn@hotmail.com>
2016-09-07 16:30:11 +0200

    still codestyle fixes

--- src/java/it/infn/ct/GridEngineInterfaceDB.java
+++ src/java/it/infn/ct/GridEngineInterfaceDB.java
@@ -28,14 +28,12 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
-//import java.util.logging.Logger;
 import org.apache.log4j.Logger;
 
 /**
  * This class interfaces the GridEngine userstracking database; it helps the
- * GridEngineInterface class
- * 
+ * GridEngineInterface class.
+ *
  * @author <a href="mailto:riccardo.bruno@ct.infn.it">Riccardo Bruno</a>(INFN)
  * @see GridEngineInterface
  */
@@ -44,281 +42,330 @@ public class GridEngineInterfaceDB {
     /*
      * Logger
      */
-    private static final Logger _log = Logger.getLogger(GridEngineInterfaceDB.class.getName());
+    /**
+     * Logger object.
+     */
+    private static final Logger LOG =
+            Logger.getLogger(GridEngineInterfaceDB.class.getName());
+    /**
+     * Line separator constant.
+     */
     public static final String LS = System.getProperty("line.separator");
+
+    /**
+     * APIServerDaemon database connection URL.
+     */
     private String connectionURL = null;
 
     /*
      * DB variables
      */
+    /**
+     * MySQL interface conneciton class.
+     */
     private Connection connect = null;
+    /**
+     * MySQL interface statemen class.
+     */
     private Statement statement = null;
+    /**
+     * MySQL interface preparedStatement class.
+     */
     private PreparedStatement preparedStatement = null;
+    /**
+     * MySQL interface resultSet class.
+     */
     private ResultSet resultSet = null;
 
     /*
      * GridEngine UsersTracking DB
      */
-    private String utdb_host;
-    private String utdb_port;
-    private String utdb_user;
-    private String utdb_pass;
-    private String utdb_name;
+    /**
+     * GridEngine UsersTrackingDB database host.
+     */
+    private String utdbHost;
+    /**
+     * GridEngine UsersTrackingDB database port number.
+     */
+    private String utdbPort;
+    /**
+     * GridEngine UsersTrackingDB database user name.
+     */
+    private String utdbUser;
+    /**
+     * GridEngine UsersTrackingDB database password.
+     */
+    private String utdbPass;
+    /**
+     * GridEngine UsersTrackingDB database name.
+     */
+    private String utdbName;
 
     /**
-     * Empty constructor for GridEngineInterface
+     * Empty constructor for GridEngineInterface.
      */
     public GridEngineInterfaceDB() {
-	_log.debug("Initializing GridEngineInterfaceDB");
+        LOG.debug("Initializing GridEngineInterfaceDB");
     }
 
     /**
-     * Constructor that uses directly the JDBC connection URL
-     * 
-     * @param connectionURL
-     *            jdbc connection URL containing: dbhost, dbport, dbuser, dbpass
-     *            and dbname in a single line
+     * Constructor that uses directly the JDBC connection URL.
+     *
+     * @param connURL - jdbc connection URL containing:
+     *                  dbhost, dbport, dbuser, dbpass
+     *                  and dbname in a single line
      */
-    public GridEngineInterfaceDB(String connectionURL) {
-	this();
-	_log.debug("GridEngineInterfaceDB connection URL:" + LS + connectionURL);
-	this.connectionURL = connectionURL;
+    public GridEngineInterfaceDB(final String connURL) {
+        this();
+        LOG.debug("GridEngineInterfaceDB connection URL:" + LS + connURL);
+        this.connectionURL = connURL;
     }
 
     /**
      * Initializing GridEngineInterface using userstrackingdb database
-     * connection settings
-     * 
-     * @param utdb_host
-     *            UsersTrackingDB database hostname
-     * @param utdb_port
-     *            UsersTrackingDB database listening port
-     * @param utdb_user
-     *            UsersTrackingDB database user name
-     * @param utdb_pass
-     *            UsersTrackingDB database user password
-     * @param utdb_name
-     *            UsersTrackingDB database name
+     * connection settings.
+     *
+     * @param host - UsersTrackingDB database hostname
+     * @param port - UsersTrackingDB database listening port
+     * @param user - UsersTrackingDB database user name
+     * @param pass - UsersTrackingDB database user password
+     * @param name - UsersTrackingDB database name
      */
-    public GridEngineInterfaceDB(String utdb_host, String utdb_port, String utdb_user, String utdb_pass,
-	    String utdb_name) {
-	this();
-	this.utdb_host = utdb_host;
-	this.utdb_port = utdb_port;
-	this.utdb_user = utdb_user;
-	this.utdb_pass = utdb_pass;
-	this.utdb_name = utdb_name;
-	prepareConnectionURL();
+    public GridEngineInterfaceDB(
+            final String host,
+            final String port,
+            final String user,
+            final String pass,
+            final String name) {
+        this();
+        this.utdbHost = host;
+        this.utdbPort = port;
+        this.utdbUser = user;
+        this.utdbPass = pass;
+        this.utdbName = name;
+        prepareConnectionURL();
     }
 
     /**
-     * Close all db opened elements: resultset,statement,cursor,connection
+     * Close all db opened elements: resultset,statement,cursor,connection.
      *
      * public void close() { closeSQLActivity();
-     * 
+     *
      * try { if (connect != null) { connect.close(); connect = null; } } catch
-     * (Exception e) { _log.fatal("Unable to close DB: '" + this.connectionURL +
+     * (Exception e) {
+     * _log.fatal("Unable to close DB: '" + this.connectionURL +
      * "'"); _log.fatal(e.toString()); }
-     * 
+     *
      * _log.info("Closed DB: '" + this.connectionURL + "'"); }
      */
 
     /**
-     * Close all db opened elements except the connection
+     * Close all db opened elements except the connection.
      */
-    public void closeSQLActivity() {
-	try {
-	    if (resultSet != null) {
-		_log.debug("closing resultSet");
-		resultSet.close();
-		resultSet = null;
-	    }
+    public final void closeSQLActivity() {
+        try {
+            if (resultSet != null) {
+                LOG.debug("closing resultSet");
+                resultSet.close();
+                resultSet = null;
+            }
 
-	    if (statement != null) {
-		_log.debug("closing statement");
-		statement.close();
-		statement = null;
-	    }
+            if (statement != null) {
+                LOG.debug("closing statement");
+                statement.close();
+                statement = null;
+            }
 
-	    if (preparedStatement != null) {
-		_log.debug("closing preparedStatement");
-		preparedStatement.close();
-		preparedStatement = null;
-	    }
+            if (preparedStatement != null) {
+                LOG.debug("closing preparedStatement");
+                preparedStatement.close();
+                preparedStatement = null;
+            }
 
-	    if (connect != null) {
-		_log.debug("closing connect");
-		connect.close();
-		connect = null;
-	    }
-	} catch (SQLException e) {
-	    _log.fatal("Unable to close SQLActivities (resultSet, statement, preparedStatement, connect)");
-	    _log.fatal(e.toString());
-	}
+            if (connect != null) {
+                LOG.debug("closing connect");
+                connect.close();
+                connect = null;
+            }
+        } catch (SQLException e) {
+            LOG.fatal("Unable to close SQLActivities "
+                    + "(resultSet, statement, preparedStatement, connect)");
+            LOG.fatal(e.toString());
+        }
     }
 
     /**
-     * Connect to the GridEngineDaemon database
-     * 
+     * Connect to the GridEngineDaemon database.
+     *
      * @return connect object
      */
     private boolean connect() {
-	try {
-	    Class.forName("com.mysql.jdbc.Driver");
-	    connect = DriverManager.getConnection(this.connectionURL);
-	} catch (Exception e) {
-	    _log.fatal("Unable to connect DB: '" + this.connectionURL + "'");
-	    _log.fatal(e.toString());
-	}
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connect = DriverManager.getConnection(this.connectionURL);
+        } catch (Exception e) {
+            LOG.fatal("Unable to connect DB: '" + this.connectionURL + "'");
+            LOG.fatal(e.toString());
+        }
 
-	_log.debug("Connected to DB: '" + this.connectionURL + "'");
+        LOG.debug("Connected to DB: '" + this.connectionURL + "'");
 
-	return (connect != null);
+        return (connect != null);
     }
 
     /**
-     * Prepare a connectionURL from detailed conneciton settings
+     * Prepare a connectionURL from detailed conneciton settings.
      */
     private void prepareConnectionURL() {
-	this.connectionURL = "jdbc:mysql://" + utdb_host + ":" + utdb_port + "/" + utdb_name + "?user=" + utdb_user
-		+ "&password=" + utdb_pass;
-	_log.debug("DBURL: '" + this.connectionURL + "'");
+        this.connectionURL =
+                  "jdbc:mysql://" + utdbHost
+                + ":" + utdbPort
+                + "/" + utdbName
+                + "?user=" + utdbUser
+                + "&password=" + utdbPass;
+        LOG.debug("DBURL: '" + this.connectionURL + "'");
     }
 
     /**
-     * Remove the given record form ActiveGridInteraction table
-     * 
-     * @param ActiveGridInteraction
-     *            id
+     * Remove the given record form ActiveGridInteraction table.
+     *
+     * @param agiId - ActiveGridInteraction id
      */
-    public void removeAGIRecord(int agi_id) {
-	if (!connect()) {
-	    _log.fatal("Not connected to database");
+    public final void removeAGIRecord(final int agiId) {
+        if (!connect()) {
+            LOG.fatal("Not connected to database");
 
-	    return;
-	}
+            return;
+        }
 
-	try {
-	    String sql;
+        try {
+            String sql;
 
-	    sql = "delete from ActiveGridInteractions" + LS + "where id = ?;";
-	    preparedStatement = connect.prepareStatement(sql);
-	    preparedStatement.setInt(1, agi_id);
-	    resultSet = preparedStatement.executeQuery();
-	} catch (SQLException e) {
-	    _log.fatal(e.toString());
-	} finally {
-	    closeSQLActivity();
-	}
+            sql = "delete from ActiveGridInteractions" + LS + "where id = ?;";
+            preparedStatement = connect.prepareStatement(sql);
+            preparedStatement.setInt(1, agiId);
+            resultSet = preparedStatement.executeQuery();
+        } catch (SQLException e) {
+            LOG.fatal(e.toString());
+        } finally {
+            closeSQLActivity();
+        }
     }
 
     /**
-     * Get ActiveGridInteraction' id field from task_id
-     * 
-     * @param taskId
-     * @return
+     * Get ActiveGridInteraction' id field from given queue command.
+     *
+     * @param geCommand - Queue command
+     * @return GridEngine UsersTracking DB ActiveGridInteraction record id
      */
-    int getAGIId(APIServerDaemonCommand geCommand) {
-	int agi_id = 0;
+    final int getAGIId(final APIServerDaemonCommand geCommand) {
+        int agiId = 0;
 
-	if (!connect()) {
-	    _log.fatal("Not connected to database");
+        if (!connect()) {
+            LOG.fatal("Not connected to database");
 
-	    return agi_id;
-	}
+            return agiId;
+        }
 
-	try {
-	    String jobDesc = geCommand.getTaskId() + "@" + geCommand.getActionInfo();
-	    String sql;
+        try {
+            String jobDesc = geCommand.getTaskId()
+                    + "@" + geCommand.getActionInfo();
+            String sql;
 
-	    sql = "select id" + LS + "from ActiveGridInteractions" + LS + "where user_description = ?;";
-	    preparedStatement = connect.prepareStatement(sql);
-	    preparedStatement.setString(1, jobDesc);
-	    resultSet = preparedStatement.executeQuery();
+            sql = "select id" + LS + "from ActiveGridInteractions" + LS
+                + "where user_description = ?;";
+            preparedStatement = connect.prepareStatement(sql);
+            preparedStatement.setString(1, jobDesc);
+            resultSet = preparedStatement.executeQuery();
 
-	    if (resultSet.next()) {
-		agi_id = resultSet.getInt("id");
-	    }
-	} catch (SQLException e) {
-	    _log.fatal(e.toString());
-	} finally {
-	    closeSQLActivity();
-	}
+            if (resultSet.next()) {
+                agiId = resultSet.getInt("id");
+            }
+        } catch (SQLException e) {
+            LOG.fatal(e.toString());
+        } finally {
+            closeSQLActivity();
+        }
 
-	return agi_id;
+        return agiId;
     }
 
     /**
-     * Return object' connection URL
-     * 
+     * Return object' connection URL.
+     *
      * @return GridEngineDaemon database connection URL
      */
-    public String getConnectionURL() {
-	return this.connectionURL;
+    public final String getConnectionURL() {
+        return this.connectionURL;
     }
 
     /**
-     * Get description of the given ActiveGridInteraction record
-     * 
-     * @param ActiveGridInteraction
-     *            id
+     * Get description of the given ActiveGridInteraction record.
+     *
+     * @param agiId - ActiveGridInteraction id
      * @return jobStatus
      */
-    public String getJobDescription(int agi_id) {
-	String uderDesc = null;
+    public final String getJobDescription(final int agiId) {
+        String uderDesc = null;
 
-	if (!connect()) {
-	    _log.fatal("Not connected to database");
+        if (!connect()) {
+            LOG.fatal("Not connected to database");
 
-	    return uderDesc;
-	}
+            return uderDesc;
+        }
 
-	try {
-	    String sql;
+        try {
+            String sql;
 
-	    sql = "select user_description" + LS + "from ActiveGridInteractions" + LS + "where id = ?;";
-	    preparedStatement = connect.prepareStatement(sql);
-	    preparedStatement.setInt(1, agi_id);
-	    resultSet = preparedStatement.executeQuery();
-	    resultSet.next();
-	    uderDesc = resultSet.getString("user_description");
-	} catch (SQLException e) {
-	    _log.fatal(e.toString());
-	} finally {
-	    closeSQLActivity();
-	}
+            sql = "select user_description" + LS
+                + "from ActiveGridInteractions" + LS
+                + "where id = ?;";
+            preparedStatement = connect.prepareStatement(sql);
+            preparedStatement.setInt(1, agiId);
+            resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            uderDesc = resultSet.getString("user_description");
+        } catch (SQLException e) {
+            LOG.fatal(e.toString());
+        } finally {
+            closeSQLActivity();
+        }
 
-	return uderDesc;
+        return uderDesc;
     }
 
-    public String getJobStatus(int agi_id) {
-	String jobStatus = null;
+    /**
+     * Retrieve the job status.
+     * @param agiId - UsersTracking ActiveGridInteraction record identifier
+     * @return Job status
+     */
+    public final String getJobStatus(final int agiId) {
+        String jobStatus = null;
 
-	if (!connect()) {
-	    _log.fatal("Not connected to database");
+        if (!connect()) {
+            LOG.fatal("Not connected to database");
 
-	    return jobStatus;
-	}
+            return jobStatus;
+        }
 
-	try {
-	    String sql;
+        try {
+            String sql;
 
-	    sql = "select status" + LS + "from ActiveGridInteractions" + LS + "where id = ?;";
-	    preparedStatement = connect.prepareStatement(sql);
-	    preparedStatement.setInt(1, agi_id);
-	    resultSet = preparedStatement.executeQuery();
+            sql = "select status" + LS
+                + "from ActiveGridInteractions" + LS
+                + "where id = ?;";
+            preparedStatement = connect.prepareStatement(sql);
+            preparedStatement.setInt(1, agiId);
+            resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                jobStatus = resultSet.getString("status");
+            }
+        } catch (SQLException e) {
+            LOG.fatal(e.toString());
+        } finally {
+            closeSQLActivity();
+        }
 
-	    if (resultSet.next()) {
-		;
-	    }
-
-	    jobStatus = resultSet.getString("status");
-	} catch (SQLException e) {
-	    _log.fatal(e.toString());
-	} finally {
-	    closeSQLActivity();
-	}
-
-	return jobStatus;
+        return jobStatus;
     }
 }
